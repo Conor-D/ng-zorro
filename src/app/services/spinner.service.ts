19f0b2bf26e8711bbb2b0f3a6a1491cd33cac965
Conor-D <conor.9277@gmail.com>
2018-07-10 11:14:43 +0800

    fix spinner scroll events.

--- src/app/services/spinner.service.ts
+++ src/app/services/spinner.service.ts
@@ -10,16 +10,24 @@ export class SpinnerService {
 
   constructor(private injector: Injector, private appRef: ApplicationRef, private resolver: ComponentFactoryResolver) {}
 
+  private tochmove(e) {
+    e.preventDefault();
+  }
+
   open() {
     this.spinner = document.createElement('spinner');
     let factory = this.resolver.resolveComponentFactory(SpinnerComponent);
     this.spinnerRef = factory.create(this.injector, [], this.spinner);
     this.appRef.attachView(this.spinnerRef.hostView);
+    document.addEventListener('touchmove', this.tochmove, false);
+    document.body.style.overflowY = 'hidden';
     document.body.appendChild(this.spinner);
   }
 
   close() {
-    document.body.removeChild(this.spinner);
     this.appRef.detachView(this.spinnerRef.hostView);
+    document.body.removeChild(this.spinner);
+    document.removeEventListener('touchmove', this.tochmove, false);
+    document.body.style.overflowY = null;
   }
 }
